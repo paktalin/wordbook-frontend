a9530a2ff088c496a0f7864344a341c104288f8a
Paktalin <likorn@ttu.ee>
2019-04-24 18:39:56 +0300

    Added login-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { WordlistComponent } from './wordlist/wordlist.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WordlistComponent
+    WordlistComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
