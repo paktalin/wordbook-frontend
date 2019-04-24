12caba426af078d5b96030ed019fc8b22d5734ce
devakiTalTech <devaki@ttu.ee>
2019-04-24 19:13:47 +0300

    configured router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {WordlistComponent} from './wordlist/wordlist.component';
+import {LoginFormComponent} from './login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent },
+  { path: '',   redirectTo: '/login', pathMatch: 'full'},
+  { path: 'words', component: WordlistComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
