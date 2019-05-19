bfd4ee084987b77c93e772c4fcf585bb15b96a18
devakiTalTech <devaki@ttu.ee>
2019-05-19 05:23:52 +0300

    permiting to /words page, need to fix navigation bar, logout deletes token when: pressed logout and/or u a located on /register /login forms, need to fix dropdown menu

---
+++ src/app/activation.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({providedIn: 'root'})
+export class ActivationService implements CanActivate {
+
+  constructor(public auth: AuthService, public router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
